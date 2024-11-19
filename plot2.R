bcb60fae258f7c32543cbffd5ba2613f9ab0c16f
UrviGarg <123055741+UrviGarg@users.noreply.github.com>
2024-11-19 18:20:36 +0530

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,17 @@
+if (!file.exists("household_power_consumption.txt")) {
+  url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(url, destfile = "dataset.zip")
+  unzip("dataset.zip")
+}
+library(data.table)
+
+data <- fread("household_power_consumption.txt", sep = ";", na.strings = "?")
+filtered_data <- data[Date %in% c("1/2/2007", "2/2/2007")]
+filtered_data[, Datetime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+# Create the plot
+png("plot2.png", width = 480, height = 480)
+plot(filtered_data$Datetime, filtered_data$Global_active_power, type = "l", 
+     xlab = "", ylab = "Global Active Power (kilowatts)", 
+     main = "")
+dev.off()
